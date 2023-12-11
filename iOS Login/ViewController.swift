3cecb246606bc4b14c6a729ae48a534956fa1fa4
Christopher Lamm <christopher.lamm@companycam.com>
2023-12-11 08:24:39 -0500

    Initial Commit

---
+++ iOS Login/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS Login
+//
+//  Created by Christopher Lamm on 12/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
