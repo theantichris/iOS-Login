d0035dd57c3fcbb981a4bc3c1600e55f9f394006
Christopher Lamm <christopher.lamm@companycam.com>
2023-12-11 09:10:50 -0500

    added navigation controller, setup landing screen title to be username

--- iOS Login/ViewController.swift
+++ iOS Login/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var usernameField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.title = usernameField.text
+    }
 }
 
