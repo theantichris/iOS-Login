edb41bd9e5f5ce8df155759f99d1e6184c4f17b6
Christopher Lamm <christopher.lamm@companycam.com>
2023-12-11 09:51:31 -0500

    set up segues for forgot username and password

--- iOS Login/ViewController.swift
+++ iOS Login/ViewController.swift
@@ -8,17 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let forgotUsernameOrPasswordSegue = "ForgottenUsernameOrPassword"
     
     @IBOutlet var usernameField: UITextField!
+    @IBOutlet var forgotUsernameButton: UIButton!
+    @IBOutlet var forgotPasswordButton: UIButton!
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        segue.destination.title = usernameField.text
+        guard let sender = sender as? UIButton else { return }
+        
+        if sender == forgotUsernameButton {
+            segue.destination.title = "Forgot Username"
+        } else if sender == forgotPasswordButton {
+            segue.destination.title = "Forgot Password"
+        } else {
+            segue.destination.title = usernameField.text
+        }
+    }
+    
+    @IBAction func forgotUsernameButtonTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: forgotUsernameOrPasswordSegue, sender: forgotUsernameButton)
+    }
+    
+    @IBAction func forgotPasswordButtonTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: forgotUsernameOrPasswordSegue, sender: forgotPasswordButton)
     }
 }
 
